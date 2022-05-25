7d67495e2e59ec2a356baa26d29dbe5f3d217f25
ywc8851 <ywc8851@naver.com>
2022-05-25 11:22:26 +0900

    feat : make TodoList component & styling

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,15 @@
 import { NextPage } from "next";
 import styled from "styled-components";
+import TodoList from "../components/TodoList";
+import { TodoType } from "../types/todo";
 
-const Container = styled.div`
-  font-style: italic;
-`;
+const todos: TodoType[] = [
+  { id: 1, text: "Next.js공부하기", color: "red", checked: false },
+  { id: 2, text: "React공부하기", color: "orange", checked: false },
+];
 
-const index: NextPage = () => {
-  return (
-    <Container>
-      <h1>testtest</h1>
-      <h2>testtest</h2>
-      <p>testtest</p>
-    </Container>
-  );
+const app: NextPage = () => {
+  return <TodoList todos={todos} />;
 };
 
-export default index;
+export default app;
